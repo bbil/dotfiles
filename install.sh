5088f86d9f87bc5210b713eb0c38faca2513c100
bbil <me@brandonbil.xyz>
2019-05-18 21:04:02 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "Copy i3 configs"
+cp -vb ~/dotfiles/i3/* ~/.config/.i3/
+ 
+echo "Copy Xdefaults"
+cp -vb ~/dotfiles/xorg/.Xdefaults ~/.Xdefaults
+
+echo "DONE!"
+echo "Things to do:"
+echo "\tRestart i3 -- or just logout and log back in"
+echo "\tManually move things from .profile over"
