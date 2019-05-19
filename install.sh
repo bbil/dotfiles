1146088fa6d4013554d66ebcccb58a9e82fb4520
Brandon Bil <me@brandonbil.xyz>
2019-05-18 21:24:34 -0700

    Add scripts folder

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ mkdir ~/.config/.i3 && cp -vb ~/dotfiles/i3/* ~/.config/.i3/
 echo "Copy Xdefaults"
 cp -vb ~/dotfiles/xorg/.Xdefaults ~/.Xdefaults
 
+echo "copying scripts"
+cp -r ~/dotfiles/.scripts/ ~
+
 echo "DONE!"
 echo "Things to do:"
+
 echo "\tRestart i3 -- or just logout and log back in"
 echo "\tManually move things from .profile over"
