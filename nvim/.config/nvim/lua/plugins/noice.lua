cf7a5532aa1403d649f3cb9405bdd8abd80e8303
bbil <me@brandonbil.xyz>
2024-06-24 20:13:06 -0400

    Disable noice for messages and notifications

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -11,7 +11,17 @@ return {
       'rcarriga/nvim-notify',
     },
     opts = {
+      -- Do not use noice for notifying
+      notify = {
+        enabled = false,
+      },
+      messages = {
+        enabled = false,
+      },
       lsp = {
+        message = {
+          enabled = false,
+        },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
@@ -26,18 +36,6 @@ return {
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true,        -- add a border to hover docs and signature help
-      },
-      routes = {
-        -- Create notification when recording a macro. Ideally I want to integrate with Lualine, but hitting an error
-        {
-          view = "notify",
-          filter = {
-            event = "msg_showmode",
-            any = {
-              { find = 'recording' },
-            },
-          },
-        },
       },
     }
   }
