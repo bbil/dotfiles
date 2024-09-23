a0bda3cb5abf982f8a1282dd4a5c27913d099e11
bbil <me@brandonbil.xyz>
2024-09-23 11:05:18 -0400

    Sync

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -20,7 +20,8 @@ return {
       require('mason-lspconfig').setup({
         -- Replace the language servers listed here
         -- with the ones you want to install
-        ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls' },
+        -- ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls' },
+        ensure_installed = { 'rust_analyzer', 'lua_ls' },
         handlers = {
           lsp_zero.default_setup,
         },
